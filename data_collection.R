e321653fd2fad0f03431294ac187acb088f5dce2
AliParky <aparkinson670@gmail.com>
2024-06-08 20:10:56 +0800

    Add code to fetch tweets using rtweet

--- data_collection.R
+++ data_collection.R
@@ -3,5 +3,11 @@ source("setup.R")
 
 # Function to collect tweets from Twitter API
 collect_tweets <- function(hashtag, n) {
+    # Use search_tweets from the rtweet package to collect tweets
+    tweets <- rtweet::search_tweets(
+        q = hashtag,
+        n = n
+    )
 
+    return(tweets)
 }
