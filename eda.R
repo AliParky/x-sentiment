d641e919fa2b358a7ccadfe21c0058a190b026fa
AliParky <aparkinson670@gmail.com>
2024-06-11 19:36:50 +0800

    Load necessary scripts and collect tweets for analysis

--- eda.R
+++ eda.R
@@ -1,2 +1,8 @@
 # Load setup.R to get necessary packages
 source("setup.R")
+
+# Load data_collection.R to get collect_tweets function
+source("data_collection.R")
+
+# Collect tweets
+tweets <- collect_tweets("#rstats", 100)
