01aa37cac9693628ac81e3436e71381786a03351
AliParky <aparkinson670@gmail.com>
2024-07-02 12:42:57 +0800

    Perform sentiment analysis on collected tweets

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -1,2 +1,8 @@
 # Load setup.R to get necessary packages
 source("setup.R")
+
+# Read the collected tweets
+tweets <- readRDS("tweets.rds")
+
+# Perform sentiment analysis
+sentiment_scores <- perform_sentiment_analysis(tweets)
