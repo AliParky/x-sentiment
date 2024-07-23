2e994ba7a7d86d8a3c8949488f39aa4a6544c49c
AliParky <aparkinson670@gmail.com>
2024-07-23 18:21:25 +0800

    Update collect_tweets to calculate tweets per day

--- data_collection.R
+++ data_collection.R
@@ -3,6 +3,11 @@ source("setup.R")
 
 # Function to collect tweets from Twitter API
 collect_tweets <- function(hashtag, n, since_date, until_date) {
+    start_date <- as.Date(since_date)
+    end_date <- as.Date(until_date)
+    total_days <- as.integer(difftime(end_date, start_date, units = "days"))
+    tweets_per_day <- ceiling(n / total_days)
+
     # Start of the collection process
     log_info(paste0("Starting tweet collection for hashtag: ", hashtag, 
                     ". Targeting ", n, " tweets from ", since_date, " to ", until_date, "."))
